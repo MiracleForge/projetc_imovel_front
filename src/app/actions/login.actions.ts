ba9edf512e6b035670c257b1f39848253afbc0cf
paulodev <paulomoreirarosado@hotmail.com>
2025-11-25 18:51:37 -0300

    Fix(loginAction): Fixed loginaction , removing token from data to validate

---
+++ src/app/actions/login.actions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { loginSchema } from "@/src/contracts/schemas/authentication/payloads.schemas";
+import { loginPayload } from "@/src/contracts/types/payloads.authentication";
+import { actionResponse } from "@/src/contracts/types/responses.core";
+import { createFetcher } from "@/src/utils/fetchData";
+import { validateTurnstileToken } from "@/src/utils/validateTurnslideToken";
+import { validateFormData } from "@/src/utils/zod/validateFormData";
+
+export async function loginAction(_prevState: any, formData: FormData): Promise<actionResponse<undefined>> {
+  const widgetToken = formData.get("cf-turnstile-response") as string | null;
+  formData.delete("cf-turnstile-response");
+
+  const payloadValided = await validateFormData(formData, loginSchema);
+  if (!payloadValided.success) {
+    return payloadValided.error;
+  }
+
+  await validateTurnstileToken(widgetToken!);
+
+  const path = "/auth/login";
+  const fetchLogin = createFetcher<loginPayload, undefined>(path, { method: "POST" });
+
+  return await fetchLogin(payloadValided.data);
+}
